20f37e9a470a1dcad15514efbba210d6890e4bb7
Gautam Sharma <gautam.sharma@gate6.com>
2023-07-11 15:30:50 +0530

    feat: added web front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // State variables for the video feed and attendance information
+  const [videoSrc, setVideoSrc] = useState(null);
+  const [attendance, setAttendance] = useState([]);
+
+  // Function to start the face recognition attendance system
+  const start = async () => {
+    // Send a request to the Flask app to start the face recognition attendance system
+    await fetch('http://127.0.0.1:5000/start');
+  };
+
+  // Function to update the video feed and attendance information
+  const update = async () => {
+    // Send a request to the Flask app to get the current video frame and attendance information
+    const response = await fetch('http://127.0.0.1:5000/update');
+    const data = await response.json();
+    
+    // Update the state variables with the received data
+    setVideoSrc(data.video_src);
+    setAttendance(data.attendance);
+  };
+
+  // Use the useEffect hook to call the update function repeatedly
+  useEffect(() => {
+    const interval = setInterval(update, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Face Recognition Attendance System</h1>
+      <button onClick={start}>Take Attendance</button>
+      <h2>Video Feed</h2>
+      <img src={videoSrc} alt="Video feed" />
+      <h2>Attendance</h2>
+      <ul>
+        {attendance.map((item, index) => (
+          <li key={index}>{item.name} - {item.time}</li>
+        ))}
+      </ul>
     </div>
   );
 }
